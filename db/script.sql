7cf57dcc68762f5e9a0eece5290c9de7e56a66d5
Aleksandr Tagilov <amtagilov@avito.ru>
2022-08-28 04:39:32 +0300

    add watcher methods, authorization, some editor methods

---
+++ db/script.sql
@@ -0,0 +1,334 @@
+DROP SCHEMA IF EXISTS cw CASCADE;
+CREATE SCHEMA cw;
+
+CREATE TABLE cw.Category
+(
+    id          SERIAL PRIMARY KEY,
+    name        varchar(30)  NOT NULL UNIQUE,
+    description varchar(300) NOT NULL
+);
+
+CREATE TABLE cw.Card
+(
+    id          SERIAL PRIMARY KEY,
+    category_id INT         NOT NULL,
+    text        varchar(30) NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES cw.Category (id)
+);
+
+CREATE TABLE cw.User
+(
+    id              SERIAL PRIMARY KEY,
+    name            varchar(100)        not null,
+    login           varchar(300) unique not null,
+    password        varchar(60) unique  not null,
+    access_level    smallint            not null,
+    token           varchar(32) unique,
+    token_timestamp timestamp
+);
+
+CREATE TABLE cw.Revision
+(
+    id         SERIAL PRIMARY KEY,
+    name       varchar(100),
+    owner_id   INT,
+    created_at timestamp,
+    FOREIGN KEY (owner_id) REFERENCES cw.User (id)
+);
+
+CREATE TABLE cw.CategoryT
+(
+    id              SERIAL PRIMARY KEY,
+    revision_id     INT,
+    category_id     INT unique,
+    new_name        varchar(30),
+    new_description varchar(30),
+    FOREIGN KEY (revision_id) REFERENCES cw.Revision (id)
+);
+ALTER TABLE cw.CategoryT
+    ADD UNIQUE (revision_id, new_name);
+
+CREATE TABLE cw.CardT
+(
+    id          SERIAL PRIMARY KEY,
+    revision_id INT,
+    category_id INT,
+    card_id     INT,
+    new_text    varchar(300),
+    FOREIGN KEY (revision_id) REFERENCES cw.Revision (id)
+);
+
+
+--хранение настроек (пока только версии базы)
+CREATE TABLE cw.key_values
+(
+    key   varchar,
+    value VARCHAR
+);
+
+--extensions
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+--функции
+--авторизация
+drop type if exists cw.auth_result CASCADE;
+CREATE type cw.auth_result AS
+(
+    id           int,
+    token        varchar(32),
+    access_level smallint
+);
+
+CREATE or replace FUNCTION cw.auth(login_in varchar, pw_in varchar)
+    RETURNS cw.auth_result
+AS
+$$
+DECLARE
+    res cw.auth_result;
+begin
+    IF EXISTS(SELECT * FROM cw.user WHERE login = login_in AND password = crypt(pw_in, password)) THEN
+        update cw.user
+        set token           = replace(gen_random_uuid()::text, '-', ''),
+            token_timestamp = now()
+        where login = login_in
+        returning id, token::text, access_level::text into res.id, res.token, res.access_level;
+    ELSE
+        select '', -1, -1 into res.token, res.access_level, res.id;
+    END IF;
+    return res;
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--регистрация, с настраиваемым access_level
+CREATE or replace FUNCTION cw.register(name_in varchar, login_in varchar, pw_in varchar, access_level_in smallint)
+    RETURNS int
+AS
+$$
+DECLARE
+    res int;
+begin
+    IF not EXISTS(SELECT * FROM cw.user WHERE login = login_in) and length(pw_in) > 8 THEN
+        insert into cw.user (name, login, password, access_level)
+        values (name_in, login_in, crypt(pw_in, gen_salt('bf')), access_level_in)
+        returning id into res;
+    ELSE
+        select -1 into res;
+    END IF;
+    return res;
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--проверка токена, возвращает либо id + уровень доступа, либо -1
+drop type if exists session_result CASCADE;
+CREATE type session_result AS
+(
+    id           int,
+    access_level smallint
+);
+
+CREATE or replace FUNCTION cw.session(token_in varchar)
+    RETURNS session_result
+AS
+$$
+declare
+    ret session_result;
+begin
+    select id, access_level
+    from cw.user
+    where token = token_in
+      and EXTRACT(EPOCH FROM now() - token_timestamp) < 86400
+    into ret.id, ret.access_level;
+    if ret.id is null then
+        select -1, -1 into ret.id, ret.access_level;
+    end if;
+    return ret;
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--получение версии
+CREATE or replace FUNCTION cw.v()
+    RETURNS varchar
+AS
+$$
+BEGIN
+    RETURN value from cw.key_values where key = 'v';
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--изменение категории или ее удаление из базы
+CREATE or replace FUNCTION cw.edit_category(
+    revision_id_in int,
+    category_id_in int,
+    name varchar default null,
+    description varchar default null
+)
+    RETURNS int
+AS
+$$
+declare
+    res int;
+begin
+    if (exists(select *
+               from cw.categoryt
+               where revision_id = revision_id_in
+                 and new_name = name
+                 and not category_id = category_id_in)) then
+        return -1;
+    end if;
+    if (not exists(select * from cw.category where id = category_id_in) and (name is null or description is null)) then
+        return -2;
+    end if;
+    if (exists(select * from cw.category where id = category_id_in) and
+        not exists(select * from cw.categoryt where category_id = category_id_in)) then
+        insert into cw.categoryt (revision_id, category_id, new_name, new_description)
+        values (revision_id_in, category_id_in, name, description)
+        returning id into res;
+    else
+        update cw.categoryT
+        set revision_id     = revision_id_in,
+            category_id     = category_id_in,
+            new_name        = name,
+            new_description = description
+        where category_id = category_id_in
+        returning id into res;
+    end if;
+    return res;
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--добавление категории
+CREATE or replace FUNCTION cw.add_category(
+    revision_id_in int,
+    name varchar,
+    description varchar
+)
+    RETURNS int
+AS
+$$
+declare
+    res int;
+begin
+    if (exists(select * from cw.categoryt where revision_id = revision_id_in and new_name = name)) then
+        return -1;
+    end if;
+    insert into cw.categoryT(revision_id, category_id, new_name, new_description)
+    values (revision_id_in, nextval('cw.category_id_seq'), name, description)
+    returning category_id into res;
+    return res;
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--удаление изменения категории
+CREATE or replace FUNCTION cw.remove_category_edition(
+    revision_id_in int,
+    category_id_in int
+) RETURNS bool AS
+$$
+begin
+    if (not exists(select * from cw.categoryt where revision_id = revision_id_in and category_id = category_id_in)) then
+        return false;
+    end if;
+    delete from cw.categoryt where revision_id = revision_id_in and category_id = category_id_in;
+    return true;
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+
+--проверка принадлоежности ревизии пользователю
+CREATE or replace FUNCTION cw.owns(
+    user_id int,
+    revision_id_in int
+)
+    RETURNS bool
+AS
+$$
+begin
+    return exists(select * from cw.revision r where r.owner_id = user_id and r.id = revision_id_in);
+END
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+--вьюхи
+--все категории с карточками для ревизии в формате json
+create view cw.revision_categories_with_cards as
+select id,
+       category_id,
+       new_name,
+       new_description,
+       revision_id,
+       (select array_to_json(array_agg(row_to_json(cards.*))) as array_to_json
+        from (select c.id, c.card_id, c.new_text
+              from cw.cardt c
+              where c.revision_id = r.revision_id
+                and c.category_id = r.category_id) cards) as cards
+from cw.categoryt r;
+
+--пользователь watcher
+-- DROP OWNED BY watcher_user;
+-- drop role if exists watcher_user;
+create role watcher_user login password '8B137DEC7A74463EB1836CA141BEADB3';
+revoke all on all tables in schema public from watcher_user;
+revoke all on all tables in schema cw from watcher_user;
+revoke execute on all functions in schema cw from watcher_user;
+grant usage on schema public to watcher_user;
+grant usage on schema cw to watcher_user;
+grant select on cw.Category, cw.Card to watcher_user;
+grant EXECUTE on function cw.v to watcher_user;
+grant EXECUTE on function cw.auth to watcher_user;
+grant EXECUTE on function cw.session to watcher_user;
+--
+-- grant EXECUTE on function cw.auth to watcher_user;
+-- grant EXECUTE on function cw.session to watcher_user;
+-- grant EXECUTE on function cw.session to watcher_user;
+-- grant EXECUTE on function cw.v to watcher_user;
+
+--пользователь editor
+-- DROP OWNED BY editor_user;
+-- drop role if exists editor_user;
+create role editor_user LOGIN password 'FBEC626988EE4A02949A95C8B5BB113A';
+revoke all on all tables in schema public from editor_user;
+revoke all on all tables in schema cw from editor_user;
+revoke execute on all functions in schema cw from editor_user;
+grant usage on schema public to editor_user;
+grant usage on schema cw to editor_user;
+grant select on cw.Category, cw.Card, cw.CardT, cw.CategoryT, cw.Revision to editor_user;
+grant select on cw.revision_categories_with_cards to editor_user;
+grant insert on cw.Revision to editor_user;
+
+grant EXECUTE on function cw.v to editor_user;
+grant EXECUTE on function cw.auth to editor_user;
+grant EXECUTE on function cw.session to editor_user;
+grant EXECUTE on function cw.edit_category to editor_user;
+grant EXECUTE on function cw.add_category to editor_user;
+grant EXECUTE on function cw.remove_category_edition to editor_user;
+grant EXECUTE on function cw.owns to editor_user;
+
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA cw TO editor_user;
+-- grant EXECUTE on function cw.auth to editor_user;
+
+
+--стартовый набор данных
+--Админ с id=0 и паролем MyPass251 по SHA-1
+insert into cw.key_values (key, value)
+values ('v', '19A634B954C14EEB97EA542D9AF0344D');
+insert into cw.category (name, description)
+values ('Простые', 'Для простой игры'),
+       ('Сложные', 'Для сложной игры');
+insert into cw.card (category_id, text)
+values (1, 'Арбуз'),
+       (1, 'Совок'),
+       (1, 'Колбаса');
+insert into cw.card (category_id, text)
+values (2, 'Автобус'),
+       (2, 'Сова'),
+       (2, 'Аэропорт');
+
+
+select cw.register('Aleksandr Tagilov'::varchar, 'amtagilov2'::varchar, 'MySimplePassword123'::varchar, 1::smallint);
+
+--примеры использования
+select a.token, a.access_level
+from cw.auth('amtagilov', 'MySimplePassword123') as a;
+select cw.session('dda5510f4b5844a3a3000710224a93df');
+SELECT s.id, s.access_level
+FROM cw.session('dda5510f4b5844a3a3000710224a93df') as s;
+select v();
---
+++ src/main.go
@@ -0,0 +1,541 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type Category struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type Revision struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	OwnerId   int    `json:"owner_id"`
+	CreatedAt string `json:"created_at"`
+}
+
+type Card struct {
+	Id   int    `json:"id"`
+	Text string `json:"text"`
+}
+
+type CategoryWithCards struct {
+	Id          int    `json:"id"`
+	Name        string `json:"task"`
+	Description string `json:"description"`
+	Cards       []Card `json:"cards"`
+}
+
+type SessionResult struct {
+	Id          int `json:"id"`
+	AccessLevel int `json:"access_level"`
+}
+
+type ConnectionData struct {
+	User     string
+	Password string
+}
+
+var watcherConnection = getConnection(ConnectionData{
+	User:     "watcher_user",
+	Password: "8B137DEC7A74463EB1836CA141BEADB3",
+})
+var editorConnection = getConnection(ConnectionData{
+	User:     "editor_user",
+	Password: "FBEC626988EE4A02949A95C8B5BB113A",
+})
+
+func getConnection(data ConnectionData) *sql.DB {
+	psqlInfo := fmt.Sprintf(
+		"host=db port=5432 user=%s password=%s dbname=postgres sslmode=disable host=db",
+		data.User, data.Password)
+	conn, err := sql.Open("postgres", psqlInfo)
+	if err != nil || conn == nil {
+		panic(errors.New(fmt.Sprintf("Cannot connect to database with user %s", data.User)))
+	}
+	return conn
+}
+
+func main() {
+	//watcherUserConn := "dbname=postgres user=watcher_user password=8B137DEC7A74463EB1836CA141BEADB3 sslmode=disable host=db" //host=db
+	//watcherUserConn := "postgresql://watcher_user:123@db:5432?sslmode=disable"
+
+	//db, err := sql.Open("postgres", watcherUserConn)
+	app := fiber.New()
+	app.Get("/check", func(c *fiber.Ctx) error {
+		return checkHandler(c)
+	})
+	app.Get("/categories", func(c *fiber.Ctx) error {
+		return getCategoriesHandler(c)
+	})
+	app.Get("/cards", func(c *fiber.Ctx) error {
+		return getCardsHandler(c)
+	})
+	app.Get("/version", func(c *fiber.Ctx) error {
+		return versionHandler(c)
+	})
+	app.Post("/auth", func(c *fiber.Ctx) error {
+		return authHandler(c)
+	})
+	app.Post("/session", func(c *fiber.Ctx) error {
+		return sessionHandler(c)
+	})
+	app.Get("/revision/list", func(c *fiber.Ctx) error {
+		return revisionListHandler(c)
+	})
+	app.Post("/revision/create", func(c *fiber.Ctx) error {
+		return revisionCreateHandler(c)
+	})
+	app.Post("/revision/category/new", func(c *fiber.Ctx) error {
+		return revisionNewCategoryHandler(c)
+	})
+	app.Post("/revision/category/edit", func(c *fiber.Ctx) error {
+		return revisionEditCategoryHandler(c)
+	})
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
+}
+
+func revisionEditCategoryHandler(ctx *fiber.Ctx) error {
+	body := struct {
+		RevisionId  int    `json:"revision_id"`
+		CategoryId  int    `json:"category_id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+	}{}
+	if err := ctx.BodyParser(&body); err != nil {
+		return fiber.NewError(400, "Please, specify 'revision_id', 'category_id' and optional 'name', 'description' via body")
+	}
+
+	session, err := authorizeRequest(ctx)
+	if err != nil {
+		return err
+	}
+	var rows *sql.Rows
+	if session.AccessLevel < 1 {
+		return fiber.ErrForbidden
+	} else if session.AccessLevel == 1 {
+		owns, err := owns(session.Id, body.RevisionId)
+		if err != nil {
+			return err
+		}
+		if !owns {
+			return fiber.ErrForbidden
+		}
+	}
+
+	if body.Name != "" && body.Description != "" {
+		rows, err = editorConnection.Query("SELECT cw.edit_category($1, $2, $3, $4)",
+			body.RevisionId, body.CategoryId, body.Name, body.Description)
+	} else if body.Name != "" {
+		rows, err = editorConnection.Query("SELECT cw.edit_category(revision_id_in := $1, category_id_in := $2, name := $3)",
+			body.RevisionId, body.CategoryId, body.Name)
+	} else if body.Description != "" {
+		rows, err = editorConnection.Query("SELECT cw.edit_category(revision_id_in := $1, category_id_in := $2, description := $3)",
+			body.RevisionId, body.CategoryId, body.Description)
+	} else {
+		rows, err = editorConnection.Query("SELECT cw.edit_category(revision_id_in := $1, category_id_in := $2)",
+			body.RevisionId, body.CategoryId)
+	}
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return databaseError(err)
+	}
+
+	var id int
+	err = rows.Scan(&id)
+	if err != nil {
+		return databaseError(err)
+	}
+
+	if id == -1 {
+		return fiber.NewError(400, "Category name are already taken")
+	} else if id == -2 {
+		return fiber.NewError(400, "Null fields for non-exist category are not allown")
+	}
+
+	return ctx.JSON(fiber.Map{"id": 3})
+}
+
+func revisionNewCategoryHandler(ctx *fiber.Ctx) error {
+	body := struct {
+		RevisionId  int    `json:"revision_id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+	}{}
+	if err := ctx.BodyParser(&body); err != nil {
+		return fiber.NewError(400, "Please, specify 'revision_id', 'name', 'description' via body")
+	}
+
+	session, err := authorizeRequest(ctx)
+	if err != nil {
+		return err
+	}
+	var rows *sql.Rows
+	if session.AccessLevel < 1 {
+		return fiber.ErrForbidden
+	} else if session.AccessLevel == 1 {
+		owns, err := owns(session.Id, body.RevisionId)
+		if err != nil {
+			return err
+		}
+		if !owns {
+			return fiber.ErrForbidden
+		}
+	}
+	rows, err = editorConnection.Query("SELECT cw.add_category($1, $2, $3)",
+		body.RevisionId, body.Name, body.Description)
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return databaseError(err)
+	}
+
+	var id int
+	err = rows.Scan(&id)
+	if err != nil {
+		return databaseError(err)
+	}
+
+	if id == -1 {
+		return fiber.NewError(400, "Duplicate name of category")
+	}
+
+	return ctx.JSON(fiber.Map{"id": id})
+}
+
+// please make sure user is authorized
+func owns(userId int, revisionId int) (bool, error) {
+	rows, err := editorConnection.Query("SELECT cw.owns($1, $2)", userId, revisionId)
+	if err != nil {
+		return false, databaseError(err)
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return false, databaseError(err)
+	}
+	var owns bool
+	err = rows.Scan(&owns)
+	if err != nil {
+		return false, databaseError(err)
+	}
+	return owns, nil
+}
+
+func revisionListHandler(ctx *fiber.Ctx) error {
+	body := struct {
+		RevisionId int `json:"revision_id"`
+	}{}
+	if err := ctx.BodyParser(&body); err != nil {
+	}
+
+	if body.RevisionId != 0 {
+		return revisionCategoriesListHandler(ctx, body.RevisionId)
+	}
+
+	session, err := authorizeRequest(ctx)
+	if err != nil {
+		return err
+	}
+	var rows *sql.Rows
+	if session.AccessLevel < 1 {
+		return fiber.ErrForbidden
+	} else if session.AccessLevel == 1 {
+		rows, err = editorConnection.Query(
+			"SELECT r.id, r.name, r.owner_id, r.created_at FROM cw.revision r WHERE owner_id = $1",
+			session.Id,
+		)
+		if err != nil {
+			return databaseError(err)
+		}
+		defer closeRows(rows)
+	} else {
+		rows, err = editorConnection.Query("SELECT r.id, r.name, r.owner_id, r.created_at FROM cw.revision r")
+		if err != nil {
+			return databaseError(err)
+		}
+		defer closeRows(rows)
+	}
+	var revisions = make([]Revision, 0)
+	for rows.Next() {
+		var row Revision
+		err := rows.Scan(&row.Id, &row.Name, &row.OwnerId, &row.CreatedAt)
+		if err != nil {
+			return databaseError(err)
+		}
+		revisions = append(revisions, row)
+	}
+	return ctx.JSON(fiber.Map{"revisions": revisions})
+}
+
+func revisionCategoriesListHandler(ctx *fiber.Ctx, revisionId int) error {
+	session, err := authorizeRequest(ctx)
+	if err != nil {
+		return err
+	}
+	var rows *sql.Rows
+	if session.AccessLevel < 1 {
+		return fiber.ErrForbidden
+	} else if session.AccessLevel == 1 {
+		owns, err := owns(session.Id, revisionId)
+		if err != nil {
+			return databaseError(err)
+		}
+		if !owns {
+			return fiber.ErrForbidden
+		}
+	}
+
+	rows, err = editorConnection.Query(
+		"select json_build_object("+
+			"'name', r.name, "+
+			"'created_at', r.created_at, "+
+			"'categories', j.list) "+
+			"from ("+
+			"select array_to_json("+
+			"array_agg("+
+			"json_build_object("+
+			"'id', c.id,"+
+			"'category_id', c.category_id,"+
+			"'name', c.new_name,"+
+			"'description', new_description,"+
+			"'cards', c.cards))) list "+
+			"from cw.revision_categories_with_cards c "+
+			"where c.revision_id = $1) j join cw.revision r on r.id = $1;",
+		revisionId)
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return databaseError(errors.New("empty function output"))
+	}
+	var resultJson string
+	err = rows.Scan(&resultJson)
+	if err != nil {
+		return databaseError(err)
+	}
+
+	ctx.Set("Content-type", "application/json; charset=utf-8")
+	return ctx.SendString(resultJson)
+}
+
+func revisionCreateHandler(ctx *fiber.Ctx) error {
+	body := struct {
+		Name string `json:"name"`
+	}{}
+	if err := ctx.BodyParser(&body); err != nil {
+		return fiber.NewError(400, "Please, specify 'name' via body")
+	}
+
+	session, err := authorizeRequest(ctx)
+	if err != nil {
+		return err
+	}
+	var rows *sql.Rows
+	if session.AccessLevel < 1 {
+		return fiber.ErrForbidden
+	}
+
+	rows, err = editorConnection.Query(
+		"INSERT INTO cw.revision (name, owner_id, created_at) VALUES ($1, $2, now()) RETURNING id",
+		body.Name, session.Id)
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return fiber.NewError(500, "Unexpected, cannot create")
+	}
+
+	var id int
+	err = rows.Scan(&id)
+	if err != nil {
+		return databaseError(err)
+	}
+
+	return ctx.JSON(fiber.Map{"id": id})
+}
+
+func closeRows(rows *sql.Rows) {
+	err := rows.Close()
+	if err != nil {
+		log.Println("Close rows error: " + err.Error())
+	}
+}
+
+func versionHandler(ctx *fiber.Ctx) error {
+	var version string
+	rows, err := watcherConnection.Query("SELECT cw.v();")
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	rows.Next()
+	err = rows.Scan(&version)
+	if err != nil {
+		return databaseError(err)
+	}
+	return ctx.SendString(version)
+}
+
+func checkHandler(ctx *fiber.Ctx) error {
+	return ctx.SendString("check1")
+}
+
+func getCategoriesHandler(ctx *fiber.Ctx) error {
+	var categories = make([]Category, 0)
+	rows, err := watcherConnection.Query("SELECT c.id, c.name, c.description FROM cw.Category c;")
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	for rows.Next() {
+		var row Category
+		err := rows.Scan(&row.Id, &row.Name, &row.Description)
+		if err != nil {
+			return databaseError(err)
+		}
+		categories = append(categories, row)
+	}
+	return ctx.JSON(fiber.Map{"categories": categories})
+}
+
+func getCardsHandler(ctx *fiber.Ctx) error {
+	var cards = make([]Card, 0)
+	var category Category
+	body := struct {
+		CategoryId int `json:"category"`
+	}{}
+	if err := ctx.BodyParser(&body); err != nil || body.CategoryId < 1 {
+		return fiber.NewError(400, "Please, specify 'category' via body")
+	}
+	rows, err := watcherConnection.Query("SELECT name, description FROM cw.category WHERE id=$1;", body.CategoryId)
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	if rows.Next() {
+		err := rows.Scan(&category.Name, &category.Description)
+		if err != nil {
+			return databaseError(err)
+		}
+	} else {
+		return fiber.NewError(400, "Category not exist")
+	}
+	rows, err = watcherConnection.Query("SELECT c.id, c.text FROM cw.card c WHERE category_id=$1;", body.CategoryId)
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	for rows.Next() {
+		var row Card
+		err := rows.Scan(&row.Id, &row.Text)
+		if err != nil {
+			return databaseError(err)
+		}
+		cards = append(cards, row)
+	}
+	if len(cards) == 0 {
+		return fiber.NewError(400, "There are no cards with this category")
+	}
+	categoryWithCards := CategoryWithCards{Name: category.Name, Description: category.Description, Cards: cards}
+	return ctx.JSON(categoryWithCards)
+}
+
+func authHandler(ctx *fiber.Ctx) error {
+	cred := struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}{}
+	if err := ctx.BodyParser(&cred); err != nil || cred.Login == "" || cred.Password == "" {
+		return fiber.NewError(400, "Please, specify 'login' and 'password' via body")
+	}
+
+	rows, err := watcherConnection.Query(
+		"SELECT a.id, a.token, a.access_level FROM cw.auth($1, $2) as a",
+		cred.Login, cred.Password,
+	)
+	if err != nil {
+		return databaseError(err)
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return fiber.ErrUnauthorized
+	}
+
+	authResult := struct {
+		Id          int    `json:"id"`
+		Token       string `json:"token"`
+		AccessLevel int    `json:"access_level"`
+	}{}
+	err = rows.Scan(&authResult.Id, &authResult.Token, &authResult.AccessLevel)
+	if err != nil {
+		panic(err)
+	}
+	return ctx.JSON(authResult)
+}
+
+func sessionHandler(ctx *fiber.Ctx) error {
+	session, err := authorizeRequest(ctx)
+	if err != nil {
+		return err
+	}
+	return ctx.JSON(*session)
+}
+
+func authorizeRequest(ctx *fiber.Ctx) (*SessionResult, error) {
+	token := ctx.GetReqHeaders()["Session"]
+	if len(token) != 32 {
+		if len(token) == 0 {
+			return nil, fiber.ErrUnauthorized
+		} else {
+			return nil, fiber.NewError(400, "Invalid token")
+		}
+	}
+	session, err := checkAuth(token)
+	if err != nil {
+		return nil, fiber.ErrUnauthorized
+	}
+	return &session, nil
+}
+
+func checkAuth(token string) (SessionResult, error) {
+	rows, err := watcherConnection.Query(
+		"SELECT s.id, s.access_level FROM cw.session($1) as s",
+		token,
+	)
+	if err != nil {
+		return SessionResult{}, err
+	}
+	defer closeRows(rows)
+	if !rows.Next() {
+		return SessionResult{}, err
+	}
+	var sessionResult SessionResult
+	err = rows.Scan(&sessionResult.Id, &sessionResult.AccessLevel)
+	if err != nil || sessionResult.Id == -1 || sessionResult.AccessLevel == -1 {
+		return SessionResult{}, errors.New("user not exists")
+	}
+	return sessionResult, err
+}
+
+func databaseError(err error) *fiber.Error {
+	log.Println(err)
+	return fiber.NewError(423, "Database error")
+}
